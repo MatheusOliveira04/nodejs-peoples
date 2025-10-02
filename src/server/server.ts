a0e716067a2a6b494a3ea0b0dd7e54ac694f7584
MatheusOliveira04 <samatheus999@gmail.com>
2025-10-02 20:26:01 -0300

    feat: Added yup message traslation

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { router } from './routes/index';
 import 'dotenv/config';
 
+import './shared/services/TranslationYup';
+
 const server = express();
 
 server.use(express.json());
