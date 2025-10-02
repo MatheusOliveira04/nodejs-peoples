98ab07fe1d8215045585963619a71853c2edf6f9
MatheusOliveira04 <samatheus999@gmail.com>
2025-10-02 20:25:11 -0300

    feat: added yup error validation

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 
 export interface ICidade {
@@ -6,18 +7,25 @@ export interface ICidade {
 }
 
 const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
-  nome: yup.string().required().min(3)
+  nome: yup.string().required().min(3),
+  cidade: yup.string().required()
 }); 
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validatedData: ICidade | undefined = undefined;
   try {
-    validatedData = await bodyValidation.validate(req.body) as ICidade;
+    validatedData = await bodyValidation.validate(req.body, { abortEarly: false }) as ICidade;
   } catch(error) {
     const yupError = error as yup.ValidationError;
+    const err: Record<string, string> = {};
 
-    return res.json({
-      errors: { default: yupError.message } 
+    yupError.inner.forEach(error => {
+      if (!error.path) return;
+       err[error.path] = error.message;
+    }); 
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      err
     });
   }
   console.log(validatedData);
